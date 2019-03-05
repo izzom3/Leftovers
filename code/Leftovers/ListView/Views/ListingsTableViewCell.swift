2abcf61653075e9ccf253d737d0e7d0974b131a0
Matt Izzo <izzom3@tcnj.edu>
2019-03-04 20:05:04 -0500

    Add files via upload

---
+++ code/Leftovers/ListView/Views/ListingsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListingsTableViewCell.swift
+//  Leftovers
+//
+//  Created by Matthew Izzo on 3/4/19.
+//  Copyright © 2019 Matthew Izzo. All rights reserved.
+//
+
+import UIKit
+
+class ListingsTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
